77789eb51d9365fa746e54ec2ddd897c17b1bbbd
Lil_SpazJoekp <joel.payne@jesassn.org>
2020-06-05 13:47:09 -0500

    added block back and changed logging

--- FlairBot/__main__.py
+++ FlairBot/__main__.py
@@ -382,19 +382,24 @@ def flairBot(reddit, subreddit, webhook, webhook_type, header, footer):
         try:
             log.info(f'r/{subreddit} | Checking last 25 flair edits...')
             for i, modAction in enumerate(subreddit.mod.log(action='editflair', limit=25), 1):
-                # submission = reddit.submission(id=modAction.target_fullname[3:])
-                # if hasattr(submission, 'link_flair_text') and submission.link_flair_text:
-                #     submissionFlair = submission.link_flair_text.lower()
-                # else:
-                #     submissionFlair = ''
-                log.info(f'r/{subreddit} | Checking {i}/25 {modAction.target_fullname} by {modAction._mod}')
-                checkModAction(modAction)
+                try:
+                    submission = reddit.submission(id=modAction.target_fullname[3:])
+                    if hasattr(submission, 'link_flair_text') and submission.link_flair_text:
+                        submissionFlair = submission.link_flair_text.lower()
+                    else:
+                        submissionFlair = ''
+                    log.info(f'r/{subreddit} | Checking {i}/25 {modAction.target_fullname} by {modAction._mod}')
+                    checkModAction(modAction)
+                except Exception as error:
+                    log.error(f'r/{subreddit} | {error}')
+                    log.exception(error)
             log.info(f'r/{subreddit} | Scanning Modlog')
             for modAction in flairRemoval.logStream():
                 checkModAction(modAction)
         except KeyboardInterrupt:
             pass
         except Exception as error:
+            log.error(f'r/{subreddit} | {error}')
             log.exception(error)
 
 class DaemonLogger:
